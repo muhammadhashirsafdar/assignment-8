cf86a3332c480581df4e05b9276488725ef929ec
muhammadhashirsafdar <139388035+muhammadhashirsafdar@users.noreply.github.com>
2023-09-15 02:19:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,243 @@
+
+
+
+
+//    !   if else & else if statement  !  //
+
+
+
+// ?  Question 1
+
+
+// var x = +prompt("Enter first number ");
+// alert(x);
+// var y = +prompt("Enter second number ");
+// alert(y);
+// if(x>y){
+//     document.write(x);
+// } else{
+//     document.write(y);
+// }
+
+
+// ?  Question 2
+
+// var x = +prompt("Enter first number ");
+// alert(x);
+// var y = +prompt("Enter second number ");
+// alert(y);
+// if(x>=y,y>=x,x<=y,y<=x){
+//     document.write(x);
+// } else{
+//     document.write(y);
+// }
+
+
+// ?  Question 3
+
+// var z= +prompt("Enter a number");
+// alert(z);
+// if(z>0){
+//     document.write(z," is positive");
+// }
+// else if (z<0){
+//     document.write(z," is negative");
+// }
+// else if (z==0){
+//     document.write(z," is zero");
+// }
+
+
+// ?  Question 4
+
+
+// var h = prompt("Enter a alpha");
+// alert(h);
+// if (h===["a","A","e","E","i","I","o","O","u","U"]){
+//     document.write(h," is vowel")
+// } else {
+//     document.write(h," is not vowel")
+// }
+
+
+// ?  Question 6
+
+// var greeting ="Good day";
+// var hour = 19;
+// if (hour < 18) {
+//     document.write(greeting);
+// } else
+//     document.write("Good evening ");
+
+
+// ?  Question 7
+
+// var total_format = "time format is 1900 = 7pm ";
+// alert(total_format);
+// var time = +prompt("Enter time");
+// alert(time);
+// if(time = '0000' && time <= 1200){
+//     document.write("Good Morning");
+// }
+// else if (time >= 1200 && time <= 1700){
+//     document.write("Good afternoon");
+// }
+// else if (time >= 1700 && time <= 2100){
+//     document.write("Good evening");
+// }
+// else if (time >= 2100 && time <= 2359){
+//     document.write("Good Night");
+// }
+
+
+// ?  Question 5
+
+
+
+
+//    !  ARRAYS  !  //
+
+
+// ?  Question 1
+
+
+// var studentnames = [];
+// alert(studentnames);
+
+// ?  Question 2
+
+// var obj = '{"studentnames":""}';
+// var json = JSON.parse(obj);
+// console.log(obj);
+// console.log(json);
+
+// ?  Question 3
+
+// var colors = ["red" , "blue" , "yellow"];
+// alert(colors)
+
+
+// ?  Question 4
+
+// var numbers = [1 , 2 , 3];
+// alert(numbers)
+
+
+// ?  Question 5
+
+
+// var boolean = [true,false];
+// alert(boolean)
+
+
+// ?  Question 6
+
+// var list = [true, "yellow", 3];
+// alert(list)
+
+// ?  Question 7
+
+// var clas = ["Qualifications :"];
+// document.write(clas);
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var classes = ["1)SSC", "2)HSC", "3)BCS", "4)BS", "5)BCOM", "6)MS", "7)M.Phil." , "8)PhD"];
+
+// var total = Math.floor(classes.length / 2);
+
+// var ssc = classes.slice(0, 1);
+// var hsc = classes.slice(1, 2);
+// var bcs = classes.slice(2, 3);
+// var bs = classes.slice(3, 4);
+// var bcom = classes.slice(4, 5);
+// var ms = classes.slice(5, 6);
+// var mphil = classes.slice(6, 7);
+// var phd = classes.slice(7, 8);
+
+// document.write(ssc);
+// document.write("<br>");
+// document.write(hsc);
+// document.write("<br>");
+// document.write(bcs);
+// document.write("<br>");
+// document.write(bs);
+// document.write("<br>");
+// document.write(bcom);
+// document.write("<br>");
+// document.write(ms);
+// document.write("<br>");
+// document.write(mphil);
+// document.write("<br>");
+// document.write(phd);
+
+
+// ?  Question 8
+
+// var score = ["score of micheal is 320.","score of john is 230.","score of tony is 480."];
+// var per = ["percentage:64%","percentage:46%","percentage:96%"];
+
+// var scoreofmicheal = score.slice(0,1);
+// var percentageofmicheal = per.slice(0,1);
+
+// var scoreofjohn = score.slice(1,2);
+// var percentageofjohn = per.slice(1,2);
+
+// var scoreoftony = score.slice(2,3);
+// var percentageoftony = per.slice(2,3);
+
+// document.write(scoreofmicheal,percentageofmicheal);
+// document.write("<br>");
+// document.write(scoreofjohn,percentageofjohn);
+// document.write("<br>");
+// document.write(scoreoftony,percentageoftony);
+
+
+// ?  Question 10
+
+// var scores = [120,480,320,230];
+// var order = scores.sort();
+// document.write("Scores of students : ",scores);
+// document.write("<br>");
+// document.write("Order scores of students : ",order);
+
+ 
+// ?  Question 11
+
+// var citynames = ["karachi","lahore","islamabad","quetta","peshawar"];
+// var selectedcities = [];
+// var one = citynames.slice(2,4);
+// document.write("cities list :");
+// document.write("<br>");
+
+// document.write(citynames);
+
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write("Selected cities list :");
+// document.write("<br>");
+
+// document.write(selectedcities,one);
+
+
+// ?  Question 12
+
+// var arr = ["This" ,  "is" ,  "my" , "cat"];
+
+// document.write("Array : ");
+// document.write("<br>");
+
+// document.write(arr)
+// document.write("<br>");
+
+// document.write("String : ");
+// document.write("<br>");
+
+// var two = arr.join(" ");
+// document.write(two);
+
+// ?  Question 13
+// ?  Question 14
+// ?  Question 15
